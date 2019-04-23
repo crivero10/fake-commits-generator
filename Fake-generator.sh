971183b8c165e91f7c8fbe4d690a44512b1fcb7b
crivero10 <2700c.ri@gmail.com>
2019-04-23 14:26:44 -0500

    hola

--- Fake-generator.sh
+++ Fake-generator.sh
@@ -2,11 +2,11 @@ days=200
 total_commit=20
 size=$((days*24*60))
 a=$(shuf -i 1-$size -n $total_commit | sort -r -n)
-git checkout -b gh-pages
+git checkout -b thebr
 for i in ${a}
 do 
 git commit --allow-empty -m "Fake commit"
 git commit --amend --allow-empty --no-edit --date "$(date -d "-$i minutes")"
 done 
-git push origin gh-pages
+git push origin thebr
 git checkout master
